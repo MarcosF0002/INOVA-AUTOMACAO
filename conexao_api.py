8f0be6ece2f314b55e50cf8b13b6d3192b55e89b
Marcos Felipe <109048913+MarcosF0002@users.noreply.github.com>
2025-08-23 14:15:14 -0300

    Update conexao_api.py

--- conexao_api.py
+++ conexao_api.py
@@ -1,16 +1,10 @@
-import os, json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import os
+import json
 
-# Lê o JSON do Secret do GitHub
-google_json = os.getenv("GOOGLE_JSON")
+google_json = os.environ.get("GOOGLE_JSON")
 creds_dict = json.loads(google_json)
-
-scopes = [
-    "https://spreadsheets.google.com/feeds",
-    "https://www.googleapis.com/auth/drive",
-]
-
-creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scopes)
+scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/drive"]
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
 client = gspread.authorize(creds)
-
