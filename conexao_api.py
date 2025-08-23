95dc894b8952d774c9dc6c7ac7611a2c71ecbddb
Marcos Felipe <109048913+MarcosF0002@users.noreply.github.com>
2025-08-23 12:50:16 -0300

    Update conexao_api.py

--- conexao_api.py
+++ conexao_api.py
@@ -1,24 +1,16 @@
-import pandas as pd
+import os, json
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-filename = "C:\\Users\\marco\\OneDrive\\Área de Trabalho\\Economia\\INOVA\\credenciais\\inova-461311-6f78bcbbcfc8.json"
+# Lê o JSON do Secret do GitHub
+google_json = os.getenv("GOOGLE_JSON")
+creds_dict = json.loads(google_json)
 
 scopes = [
     "https://spreadsheets.google.com/feeds",
     "https://www.googleapis.com/auth/drive",
 ]
 
-creds = ServiceAccountCredentials.from_json_keyfile_name(
-    filename = filename,
-    scopes = scopes
-)
-
+creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scopes)
 client = gspread.authorize(creds)
-print(client)
-
-planilha_Startups = client.open(
-    title = "PORTAL DA INOVAÇÃO E STARTUPS", 
-    folder_id="1JktHXPDmaY3xQGlXAkRWx2z4ZmLOXCCY",
-    )
 
