c9da2833d98ad6c0912b95b49047cfd1dac61b71
Marcos Felipe <109048913+MarcosF0002@users.noreply.github.com>
2025-08-23 14:18:47 -0300

    Update conexao_api.py

--- conexao_api.py
+++ conexao_api.py
@@ -1,10 +1,21 @@
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
 import os
 import json
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
-google_json = os.environ.get("GOOGLE_JSON")
+# Lê o JSON do secret
+google_json = os.environ.get("GSHEETS_CREDENTIALS_JSON")
+if not google_json:
+    raise ValueError("O secret GSHEETS_CREDENTIALS_JSON não está definido!")
+
 creds_dict = json.loads(google_json)
-scope = ["https://spreadsheets.google.com/feeds","https://www.googleapis.com/auth/drive"]
+
+# Define o escopo do Google Sheets
+scope = ["https://spreadsheets.google.com/feeds",
+         "https://www.googleapis.com/auth/drive"]
+
+# Cria credenciais a partir do dicionário
 creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
+
+# Autoriza e cria o cliente do gspread
 client = gspread.authorize(creds)
